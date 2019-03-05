fc7734dcb99d3eea9f8037605548f814894fcefc
cloud4288 <philipmarkgutierrez@gmail.com>
2019-03-05 21:37:55 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# File Change Last Modified Time
+This is a simple example of an existing file changed it's last modified date
+
+## Reference
+https://www.mkyong.com/java/how-to-change-the-file-last-modified-date-in-java/
---
+++ src/main/java/com/groundgurus/lastmodified/FileChangeLastModifiedTimeApp.java
@@ -0,0 +1,33 @@
+package com.groundgurus.lastmodified;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class FileChangeLastModifiedTimeApp {
+    private static final Logger log = Logger.getLogger(FileChangeLastModifiedTimeApp.class.getName());
+
+    public static void main(String[] args) {
+        String filePath = System.getProperty("user.home") + File.separator + "myfile.txt";
+        File myFile = new File(filePath);
+
+        // Create the file if it doesn't exists
+        if (!myFile.exists()) {
+            try {
+                myFile.createNewFile();
+            } catch (IOException e) {
+                log.log(Level.SEVERE, e.getMessage());
+            }
+        }
+
+        // set to next month
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.MONTH, 2);
+        myFile.setLastModified(cal.getTimeInMillis());
+
+        log.info("Last Modified (After): " + new Date(myFile.lastModified()));
+    }
+}
